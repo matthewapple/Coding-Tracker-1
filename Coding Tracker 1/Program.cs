26b22508c742c250a85a23ee899efbd19da25997
Matt Sella <guppyflies@gmail.com>
2022-04-22 09:51:29 -0600

    Add project files.

---
+++ Coding Tracker 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+
+
+namespace Coding_Tracker_1
+{
+    internal class Program
+    {
+        public static string connectionString = ConfigurationManager.AppSettings["k1"];
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"{connectionString}");
+        }
+    }
+}
